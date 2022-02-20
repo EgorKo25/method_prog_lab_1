995281f45105742f075b77862973019c54e79eeb
Egor <egorko25@gmail.com>
2022-02-20 16:01:37 +0300

    strat working

---
+++ First_C_part/solution_first_part.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
+int BytePower(int value1, int value2)
+{
+    int res = value1 << value2;
+    return res;
+}
+int Sqrt2(int value)
+{
+    
+}

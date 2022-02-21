984d239bf8109d1aa4ade140977e7b73ceab2af2
Egor <egorko25@gmail.com>
2022-02-21 13:56:01 +0300

    Bulshit isn't working

--- First_C_part/solution_first_part.cpp
+++ First_C_part/solution_first_part.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 
-int main(int argc, char **argv)
+void Sqrt2(int value, int *power)
 {
-    return 0;
+
+    while (value != 1)
+    {
+        value /= 2;
+        power++;
+    }
 }
 int BytePower(int value1, int value2)
 {
-    int res = value1 << value2;
+    int power = 0;
+    int *ptr_power = &power;
+
+    Sqrt2(value2, ptr_power);
+
+    int res = value1 << power;
     return res;
 }
-int Sqrt2(int value)
+
+void GetData(int *value1, int *value2)
 {
-    
+    std::cout << "Enter the first value: \t";
+    std::cin >> *value1;
+
+    std::cout << "Enter the second value: \t";
+    std::cin >> *value2;
+}
+
+int main(int argc, char **argv)
+{
+    int value1, value2;
+    int *ptr_value1 = &value1;
+    int *ptr_value2 = &value2;
+    GetData(ptr_value1, ptr_value2);
+    std::cout << ptr_value1 << "\t" << ptr_value2 << std::endl;
+
+    return 0;
 }

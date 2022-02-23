7f5cdcb3ce2098de93ab300cf7097ad971f54416
Egor <egorko25@gmail.com>
2022-02-23 11:26:56 +0300

    Finish the first part

--- First_C_part/solution_first_part.cpp
+++ First_C_part/solution_first_part.cpp
@@ -2,21 +2,20 @@
 
 void Sqrt2(int value, int *power)
 {
-
     while (value != 1)
     {
         value /= 2;
-        power++;
+        (*power)++;
     }
 }
 int BytePower(int value1, int value2)
 {
     int power = 0;
-    int *ptr_power = &power;
 
-    Sqrt2(value2, ptr_power);
-
+    Sqrt2(value2, &power);
+   
     int res = value1 << power;
+ 
     return res;
 }
 
@@ -32,10 +31,9 @@ void GetData(int *value1, int *value2)
 int main(int argc, char **argv)
 {
     int value1, value2;
-    int *ptr_value1 = &value1;
-    int *ptr_value2 = &value2;
-    GetData(ptr_value1, ptr_value2);
-    std::cout << ptr_value1 << "\t" << ptr_value2 << std::endl;
+
+    GetData(&value1, &value2);
+    BytePower(value1, value2);
 
     return 0;
 }

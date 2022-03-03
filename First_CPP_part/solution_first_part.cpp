50345d5a36dcb5505b6862e4f348a3725b61b5d8
Egor <egorko25@gmail.com>
2022-03-03 12:01:02 +0300

    Fix IT

--- First_CPP_part/solution_first_part.cpp
+++ First_CPP_part/solution_first_part.cpp
@@ -1,53 +1,61 @@
 #include <iostream>
 
-void Sqrt2(int value, int *power)
+template <typename T>
+T GetAnChaeckData()
 {
-    try
+    T temp;
+    std::cin >> temp;
+    while (cin.fail() || cin.get() != '\n')
     {
-        while (value != 1)
-        {
-            value /= 2;
-            (*power)++;
-        }
+        cin.clear(); // Resets all bits of flow, Ie the flow becomes good
+        cin.ignore(9999, '\n');
+        cin.sync(); //Delete data from Buffer
+        STD::cout << "Enter again: ";
+        STD::cin >> temp;
     }
-    catch (...)
-    {
-        std::cout << "Error! Unusable value!";
-    }
+    return temp;
 }
+int isPow(int pow)
+{
+    int tmp = 0;
+
+    if (((pow % 2) == 1) & (pow != 1))
+        return -1;
+
+    while (pow != 1)
+    {
+        pow /= 2;
+        tmp++;
+    }
+
+    if (pow > 1)
+        return -1;
+
+    return tmp;
+}
+
 int BytePower(int value1, int value2)
 {
-    register int power = 0;
+    register int res = 0;
+    value2 = isPow(value2);
 
-    Sqrt2(value2, &power);
-
-    register int res = value1 << power;
+    if(value2 != -1)
+    {
+        res = value1 << value2;
+    }else{
+        std::cout << "Value2 is not a power of 2";
+    }
 
     return res;
-}
-
-void GetData(int *value1, int *value2)
-{
-    try
-    {
-        std::cout << "Enter the first value: \t";
-        std::cin >> *value1;
-
-        std::cout << "Enter the second value: \t";
-        std::cin >> *value2;
-    }
-    catch(...)
-    {
-        std::cout << "Error! Unusable value!";
-    }
-    
 }
 
 int main(int argc, char **argv)
 {
     register int value1, value2;
 
-    GetData(&value1, &value2);
+    value1 = GetAnChaeckData<int>();
+    value2 = GetAnChaeckData<int>();
+    
     register int res = BytePower(value1, value2);
 
     std::cout << "Result of the bit mul:\t" << res << std::endl;
